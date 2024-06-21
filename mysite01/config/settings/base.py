582cd97ef7dc3259d3e09a90bcbffaf1d24c59a4
JeongBeom <whwjdqja58@naver.com>
2024-06-21 15:50:24 +0900

    Change logger

--- mysite01/config/settings/base.py
+++ mysite01/config/settings/base.py
@@ -192,5 +192,9 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
